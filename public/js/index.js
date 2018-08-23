1ce8f9c1cf72ff01ad14cd9d27d178f5a02f3b49
Mark <marklureal@gmail.com>
2018-08-23 16:04:31 +0800

    add css for chat

--- public/js/index.js
+++ public/js/index.js
@@ -30,10 +30,14 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextbox = $('[name=message]')
+
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
-    }, function () { });
+        text: messageTextbox.val()
+    }, function () { 
+        messageTextbox.val('');
+    });
 });
 
 const locationButton = $('#send-location');
@@ -42,13 +46,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser');
     }
 
-
+    locationButton.attr('disabled', 'disabled').text('Sending location....');
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
+
         alert('Unable to fectch location');
     });
 });
